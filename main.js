bccc7de481b21e6bea4aa3d0239bcfe6e0d375e6
Daria <dariashvydka@gmail.com>
2017-09-26 12:09:33 +0300

    final global refactoring

--- main.js
+++ main.js
@@ -6,20 +6,20 @@ function makeSlide(url, className) {
 `)
 };
 
-var colors = ['images/boy_1.png', 'images/boy_2.png'];
+var images = ['images/boy_1.png', 'images/boy_2.png'];
 
 function slider() {
     const slidesCount = 3;
-    let color = 0
+    let image = 0
     for (let i = 0; i < slidesCount; i++) {
-        $('.slider__item--container').append(makeSlide(colors[+color]))
-        color = !(color)
+        $('.slider__item--container').append(makeSlide(images[+image]))
+        image = !(image)
     };
     $('.button__right').on('click', () => {
         $('.slider__item').last().addClass('move-right');
         window.setTimeout(function() {
-            $('.slider__item--container').prepend(makeSlide(colors[+color], 'move-left abs'))
-            color = !(+color)
+            $('.slider__item--container').prepend(makeSlide(images[+image], 'move-left abs'))
+            image = !(+image)
             window.setTimeout(function() {
                 $('.slider__item:eq( 1 ), .slider__item:eq( 2 )').css({right: "187px"});
                 $('.slider__item').first().removeClass('move-left')
@@ -34,14 +34,14 @@ function slider() {
     $('.button__left').on('click', () => {
         $('.slider__item').first().addClass('move-left');
         window.setTimeout(function() {
-            $('.slider__item--container').append(makeSlide(colors[+color], 'move-right abs'))
-            color = !(+color)
+            $('.slider__item--container').append(makeSlide(images[+image], 'move-right abs'))
+            image = !(+image)
             window.setTimeout(function() {
                 console.log($('.slider__item:eq( 1 ), .slider__item:eq( 2 )'));
                 $('.slider__item:eq( 1 ), .slider__item:eq( 2 )').css({left: "187px"});
                 $('.slider__item').last().removeClass('move-right abs')
                 $('.slider__item').first().remove()
-                $('.slider__item:eq( 0 ), .slide:eq( 1 )').animate({left: "0px"}, 500, function() {
+                $('.slider__item:eq( 0 ), .slider__item:eq( 1 )').animate({left: "0px"}, 500, function() {
                     $('.slider__item').css({'left': ''})
                 });
             }, 200)
